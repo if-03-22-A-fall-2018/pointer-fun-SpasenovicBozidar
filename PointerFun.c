18efcdf229136e204a3bdb9ce22e6e24243afdd6
SpasenovicBozidar <spasenovic2002@gmail.com>
2018-10-14 12:37:46 +0200

    Finish Version of PointerFun

--- PointerFun.c
+++ PointerFun.c
@@ -10,8 +10,9 @@ void print_integers(int int_value, int *(int_pointer)){
   printf("Got an integer value %d and an address to an integer with value %d\n", int_value, int_pointer);
 }
 void change_integers(int int_value, int *(int_pointer)){
-  int_value =30;
-  *int_pointer = 40;
+  printf("Enter a number: ");
+  scanf("%d", &int_value);
+
 
     print_integers(int_value, int_pointer);
 }
